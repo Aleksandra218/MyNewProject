58ecfda1bc1b003104c2df7924a27d4f4527bdab
Aleksandra <alya.karas218@gmail.com>
2023-06-10 17:10:07 +0700

    * My first commit

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+//public class Car {
+//
+//    String model;
+//    int maxSpeed;
+//    int yearOfManufacture;
+//
+//    public Car(String model, int maxSpeed, int yearOfManufacture) {
+//        this.model = model;
+//        this.maxSpeed = maxSpeed;
+//        this.yearOfManufacture = yearOfManufacture;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (maxSpeed == ((Car)obj).maxSpeed) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Car ferrari = new Car("Ferrari 360 Spider", 280, 1996);
+//        Car ferrariTwin = new Car("Ferrari 360 Spider", 280, 1996);
+//        System.out.println(ferrari.equals(ferrariTwin));
+//    }
+//}
---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+    public int age =10;
+    public String str = "Привет";
+
+    public static void main(String[] args) {
+        
+    }
+
+    public void anInt() {
+        Solution cat = new Solution();
+
+        int age1 = cat.age;
+        System.out.println(cat);
+
+//        age = 15;
+//        str = "Привет";
+    }
+}
+
+
+//public class Solution {
+//    public static void main(String args[]) {
+//        new ChildClass();
+//        System.out.println();
+//        new ChildClass();
+//
+//    }
+//}
